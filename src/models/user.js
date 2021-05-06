d8ff027d345cd43462f0658ae4b609e1e0a52031
Jose Antonio Marín <jamarin02@gmail.com>
2021-05-06 21:30:58 +0200

    [UPD] Validate user data before save

--- src/models/user.js
+++ src/models/user.js
@@ -3,9 +3,10 @@ const Validator = require('fastest-validator')
 const v = new Validator()
 
 const check = v.compile({
-    username: { type: "string", min: 4, max: 255 },
-    password: { type: "string", min: 4, max: 255 },
-    email: { type: "email" }
+    username: "string|min:4|max:255",
+    password: "string|min:4|max:255",
+    email: "email",
+    active: "boolean|optional"
 })
 
 module.exports = {
